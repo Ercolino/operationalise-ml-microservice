23ff34fa7cd71ac2fb688e0d1d5ec65bc7c84d6d
Fabrizio Ciccarese <fabrizio.ciccarese.learning@gmail.com>
2020-06-28 01:45:20 +0000

    Add kubernetes output

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,17 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath="fciccarese/udacity-project-four:latest"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl create deployment udacity-project --image=$dockerpath
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods --all-namespaces
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl expose deployment udacity-project --type="NodePort" --port=80
+kubectl port-forward deployment/udacity-project 8000:80
